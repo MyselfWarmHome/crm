57689216aa84ebdcd1962262a14174e2ac1f8431
MyselfWarmHome <wjl1506269491@163.com>
2017-06-11 03:34:40 +0800

    整理包结构

---
+++ src/com/itheima/dao/CustomerDao.java
@@ -0,0 +1,7 @@
+package com.itheima.dao;
+
+/**
+ * Created by dell on 2017/6/11.
+ */
+public interface CustomerDao {
+}
---
+++ src/com/itheima/service/CustomerService.java
@@ -0,0 +1,7 @@
+package com.itheima.service;
+
+/**
+ * Created by dell on 2017/6/11.
+ */
+public interface CustomerService {
+}
