ccef23fa2726f68fa479322cd9ec162e895f8687
MyselfWarmHome <wjl1506269491@163.com>
2017-06-11 04:23:59 +0800

    整理jar包

--- src/com/itheima/dao/CustomerDao.java
+++ src/com/itheima/dao/CustomerDao.java
@@ -4,4 +4,5 @@ package com.itheima.dao;
  * Created by dell on 2017/6/11.
  */
 public interface CustomerDao {
+    public void save();
 }
--- src/com/itheima/dao/CustomerDaoImpl.java
+++ src/com/itheima/dao/CustomerDaoImpl.java
@@ -4,4 +4,7 @@ package com.itheima.dao;
  * Created by dell on 2017/6/11.
  */
 public class CustomerDaoImpl implements CustomerDao{
+    public void save(){
+
+    };
 }
